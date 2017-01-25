51bcfdf60698e10b8eecbda9f1b9aa48a8ed55b9
mike <mike706574@gmail.com>
2017-01-24 22:58:53 -0600

    Reorder properties.

--- src/leiningen/new/slate/templates/project.clj
+++ src/leiningen/new/slate/templates/project.clj
@@ -1,7 +1,6 @@
 (defproject {{full-name}} "0.1.0-SNAPSHOT"
   :description "TODO"
   :url "TODO"
-
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
@@ -9,7 +8,7 @@
                  [com.stuartsierra/component "0.3.1"]]
   :profiles {:uberjar {:aot :all
                        :main {{full-name}}.main}
-             :dev {:target-path "target/dev"
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
-                   :source-paths ["dev"]}}
+             :dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}}
   :repl-options {:init-ns user})
