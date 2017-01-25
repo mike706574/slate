bb1753c152e38525da7752c6da93b2210c94ab8e
mike <mike706574@gmail.com>
2017-01-24 22:57:01 -0600

    Only enable AOT in uberjar profile to fix namespace refresh. Remove potentially unwanted dev dependencies.

--- src/leiningen/new/slate/templates/project.clj
+++ src/leiningen/new/slate/templates/project.clj
@@ -2,16 +2,14 @@
   :description "TODO"
   :url "TODO"
 
-  :aot :all
-  :main {{full-name}}.main
-
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [com.taoensso/timbre "4.8.0"]
                  [com.stuartsierra/component "0.3.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/data.json "0.2.6"]
-                                  [clj-http "3.4.1"]]
+  :profiles {:uberjar {:aot :all
+                       :main {{full-name}}.main}
+             :dev {:target-path "target/dev"
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}}
   :repl-options {:init-ns user})
