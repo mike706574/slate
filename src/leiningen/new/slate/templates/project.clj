0cf51b4385c533db55a5fffe8768807b873db473
mike <mike706574@gmail.com>
2017-03-24 00:32:37 -0500

    Add dep.

--- src/leiningen/new/slate/templates/project.clj
+++ src/leiningen/new/slate/templates/project.clj
@@ -10,5 +10,6 @@
                        :main {{full-name}}.main}
              :dev {:source-paths ["dev"]
                    :target-path "target/dev"
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}}
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/test.check "0.9.0"]]}}
   :repl-options {:init-ns user})
