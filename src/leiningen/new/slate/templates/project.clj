ece06b2741cc67a4eb3a6df945476fe0f720f21b
mike <mike706574@gmail.com>
2017-05-11 18:35:28 -0500

    Update deps. Specify license.

--- src/leiningen/new/slate/templates/project.clj
+++ src/leiningen/new/slate/templates/project.clj
@@ -1,10 +1,11 @@
 (defproject {{full-name}} "0.1.0-SNAPSHOT"
   :description "TODO"
   :url "TODO"
-  :license {:name "TODO: Choose a license"
-            :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [com.taoensso/timbre "4.8.0"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.3.2"]]
   :profiles {:uberjar {:aot :all
                        :main {{full-name}}.main}
