a1ba884913d5b393b1ecae9901ef125e41e652f2
mike <mike706574@gmail.com>
2017-05-11 18:26:28 -0500

    Remove unwanted gen-class.

--- src/leiningen/new/slate/templates/system.clj
+++ src/leiningen/new/slate/templates/system.clj
@@ -1,6 +1,5 @@
 (ns {{full-name}}.system
-  (:require [com.stuartsierra.component :as component])
-  (:gen-class :main true))
+  (:require [com.stuartsierra.component :as component]))
 
 (defn system
   "Returns a new instance of the whole application."
