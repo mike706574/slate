af4889f008ef83ab0b8e2cb47a69d201c7c499a7
Mike <mike706574@gmail.com>
2017-08-15 18:27:13 -0500

    Use alpha17.

--- src/leiningen/new/slate/templates/project.clj
+++ src/leiningen/new/slate/templates/project.clj
@@ -1,10 +1,11 @@
 (defproject {{full-name}} "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
+  :description "Describe me!"
+  :url "https://github.com/mike706574/{{full-name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/spec.alpha "0.1.123"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.3.2"]]
   :profiles {:uberjar {:aot :all
